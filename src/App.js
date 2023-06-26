13ee3fde6a9e30faa582a547aa0f447909deab66
Victor <vmedina1014@gmail.com>
2023-06-26 09:02:29 -0700

    Initial React framework

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/about'
+function App() {
+  return (
+    <div className="App">
+      <About></About>
+      
+    </div>
+  );
+}
+
+export default App;
