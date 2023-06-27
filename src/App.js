766d14e25fd28e17f342656deab432558b3f0e68
Victor Medina <vmedina1014@gmail.com>
2023-06-27 08:14:08 -0700

    Separated entire sidebar into individual components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import About from './components/about'
 import Experience from './components/experience'
-import Sidebar from './components/sidebar'
 import Skills from './components/skills';
+import Education from './components/education'
+import Certifications from './components/certifications';
+import Contact from './components/contact';
+import Resume_Download from './components/resume_download'
 
 function App() {
   return (
     <div class="flex-container">
 
-      
+
       <div id="sidebar">
-      <About></About>
-      <hr></hr>
-      <Skills></Skills>
-      <hr></hr>
-      <Sidebar></Sidebar>
+        <About></About>
+        <hr></hr>
+        <Skills></Skills>
+        <hr></hr>
+        <Education></Education>
+        <hr></hr>
+        <Certifications></Certifications>
+        <hr></hr>
+        <Contact></Contact>
+        <hr></hr>
+        <Resume_Download></Resume_Download>
       </div>
+
 
       <Experience></Experience>
     </div>
---
+++ src/components/resume_download.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+
+export default class contact extends Component {
+  render() {
+    return (
+      <div>
+
+
+<h3 className="sidebar-section">Download my Resumes</h3>
+        <a href="pdfs/Medina_Victor_Resume-1.pdf" target="_blank">
+          Software Engineer Resume
+        </a>
+        <br />
+        <a href="pdfs/Medina_Victor_Resume_SA_Apri2021.pdf" target="_blank">
+          Structural Analysis Engineer Resume
+        </a></div>
+    )
+  }
+}
